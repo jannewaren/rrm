34048dbff5c7c6f0391c91af994558c18685b162
Janne Warén <janne.waren@visma.com>
2019-03-23 15:34:17 +0200

    Separate tests for when we want and what do don't

--- spec/rrm_spec.rb
+++ spec/rrm_spec.rb
@@ -3,8 +3,8 @@ RSpec.describe Rrm do
     expect(Rrm::VERSION).not_to be nil
   end
 
-  describe '#fetch_all_ruby_versions'  do
-    it 'gets all', vcr: { cassette_name: 'fetch_all_ruby_versions' } do
+  describe '#all_ruby_versions', vcr: { cassette_name: 'all_ruby_versions' } do
+    it 'include released normal versions' do
       expect(described_class.all_ruby_versions).is_a? Array
       expect(described_class.all_ruby_versions).to include '1.9.3'
       expect(described_class.all_ruby_versions).to include '2.1.2'
@@ -12,6 +12,9 @@ RSpec.describe Rrm do
       expect(described_class.all_ruby_versions).to include '2.3.3'
       expect(described_class.all_ruby_versions).to include '2.5.1'
       expect(described_class.all_ruby_versions).to include '2.6.2'
+    end
+
+    it 'does not include rc or preview versions' do
       expect(described_class.all_ruby_versions).to_not include '2.4.0-preview1'
       expect(described_class.all_ruby_versions).to_not include '2.6.0-rc2'
     end
